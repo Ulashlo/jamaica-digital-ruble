694b68f918b041144cf9c0060785d38b31e63402
Nikolskiy Vlad <Nikolskiy.2000@mail.ru>
2022-02-23 01:02:51 +0300

    Добавил тесты

--- Backend/src/main/java/Main.java
+++ Backend/src/main/java/Main.java
@@ -6,9 +6,9 @@ import org.web3j.tx.gas.StaticGasProvider;
 import java.math.BigInteger;
 
 public class Main {
-    private static String contractAddress = "0x4BE879dBDFeca9E3d3206AA06f8bdC97360ca858";
-    private static String address = "0x3BDF6B821c2BBb7C2e405DfA21686672225E56f4";
-    private static String key = "e773503ad9ce1d5505594f9e299291afd50dc7c62d00cc84f9de9a9cf41b77a9";
+    private static String contractAddress = "0xB2c363333EB46c865C1ebaDf7411c443Ac2E0d9F";
+    private static String address = "0xB8e04865A2E385739508B430da58A6625514980a";
+    private static String key = "f575ea7bdcad594780c35082fc39cddb0f1c97df06eade2208277c49beef74d6";
 
     public static void main(String[] args) throws Exception {
         Web3j web3j = Web3j.build(new HttpService("http://localhost:7545/"));
---
+++ Contracts/test/JDR.test.js
@@ -0,0 +1,41 @@
+const JDR = artifacts.require("JDR");
+
+contract('JDR', (accounts) => {
+    let jdr;
+    let owner = accounts[0];
+
+    beforeEach('Setup JDR', async () => {
+        jdr = await JDR.deployed();
+    })
+
+    describe('#owner', () => {
+        it('Should have correct owner', async () => {
+            const expected_owner = await jdr.owner.call();
+
+            assert.equal(expected_owner, owner, "Wrong owner");
+        });
+    })
+
+    describe('#mint', () => {
+        it('Should mint new jdr successfully', async () => {
+            await jdr.mint.call(1500);
+
+            const minted = await jdr.balances.call(owner);
+            console.log(minted.amount)
+            assert.equal(1500, minted.amount, "Wrong amount of minted jdr");
+        });
+    })
+
+    describe('#transfer', () => {
+        before('Mint JDR', async () => {
+            await jdr.mint.call(3000);
+        })
+
+        it('Should transfer jdr successfully', async () => {
+            await jdr.transfer.call(accounts[1], 1000);
+
+            const transfered = await jdr.balances.call(accounts[1]);
+            assert.equal(950, transfered.amount, "Wrong amount of minted jdr");
+        });
+    })
+});
